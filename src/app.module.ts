658d4cf53f7dfa275e4c336791b088ed71a7321b
Qaisar Waheed <qwaheed@intelligentsoftware.net>
2023-02-09 19:45:31 +0500

    hello

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
+
+const DB = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'test',
+  entities: [User],
+  synchronize: true,
+});
+
+@Module({
+  imports: [DB, UserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
